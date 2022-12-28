24c4c8b4c9afca9ed9ee446cfac92964a03362a5
Yu Chien <yuuchien.com>
2022-12-28 12:32:31 +0800

    weather icon changing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 // ./src/App.js
 import React from 'react';
 import styled from '@emotion/styled';
+import WeatherIcon from './components/WeatherIcon';
 
 // ThemeProvider 跟 useState
 import { ThemeProvider } from '@emotion/react';
 import { useState, useEffect, useCallback } from 'react';
 
 // 載入圖示
-import { ReactComponent as DayCloudyIcon } from './images/day-cloudy.svg';
 import { ReactComponent as AirFlowIcon } from './images/airFlow.svg';
 import { ReactComponent as RainIcon } from './images/rain.svg';
 import { ReactComponent as RefreshIcon } from './images/refresh.svg';
@@ -110,10 +110,6 @@ const Rain = styled.div`
     height: auto;
     margin-right: 30px;
   }
-`;
-
-const DayCloudy = styled(DayCloudyIcon)`
-  flex-basis: 30%;
 `;
 
 const Refresh = styled.div`
@@ -275,7 +271,7 @@ const App = () => {
           <Temperature>
           {Math.round(temperature)} <Celsius>°C</Celsius>
           </Temperature>
-          <DayCloudy />
+          <WeatherIcon />
         </CurrentWeather>
         <AirFlow><AirFlowIcon /> {windSpeed} m/h </AirFlow>
         <Rain><RainIcon /> {rainPossibility}% </Rain>
