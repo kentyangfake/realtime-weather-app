020096501215830dedd6432838b967540c35ecec
Yu Chien <yuuchien.com>
2022-12-30 10:46:15 +0800

    refactorying

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import styled from '@emotion/styled';
-import WeatherIcon from './components/WeatherIcon';
 import { getMoment } from './utils/helper';
+import WeatherCard from './views/WeatherCard';
 
 // ThemeProvider 跟 useState
 import { ThemeProvider } from '@emotion/react';
 import React, { useState, useEffect, useCallback, useMemo } from 'react';
 
-// 載入圖示
-import { ReactComponent as AirFlowIcon } from './images/airFlow.svg';
-import { ReactComponent as RainIcon } from './images/rain.svg';
-import { ReactComponent as RefreshIcon } from './images/refresh.svg';
-import { ReactComponent as LoadingIcon } from './images/loading.svg';
+
 
 //跨瀏覽器處理工具(時間格式)
 import dayjs from 'dayjs';
@@ -44,99 +40,7 @@ const Container = styled.div`
   font-family: system-ui;
 `;
 
-const WeatherCard = styled.div`
-  position: relative;
-  min-width: 360px;
-  box-shadow: ${({ theme }) => theme.boxShadow};
-  background-color: ${({ theme }) => theme.foregroundColor};
-  box-sizing: border-box;
-  padding: 30px 15px;
-`;
 
-const Location = styled.div`
-  font-size: 28px;
-  color: ${({ theme }) => theme.titleColor};
-  margin-bottom: 20px;
-`;
-
-const Description = styled.div`
-  font-size: 16px;
-  color: ${({ theme }) => theme.textColor};
-  margin-bottom: 30px;
-`;
-
-const CurrentWeather = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 30px;
-`;
-
-const Temperature = styled.div`
-  color: ${({ theme }) => theme.temperatureColor};
-  font-size: 96px;
-  font-weight: 300;
-  display: flex;
-`;
-
-const Celsius = styled.div`
-  font-weight: normal;
-  font-size: 42px;
-`;
-
-const AirFlow = styled.div`
-  display: flex;
-  align-items: center;
-  font-size: 16x;
-  font-weight: 300;
-  color: ${({ theme }) => theme.textColor};
-  margin-bottom: 20px;
-  svg {
-    width: 25px;
-    height: auto;
-    margin-right: 30px;
-  }
-`;
-
-const Rain = styled.div`
-  display: flex;
-  align-items: center;
-  font-size: 16x;
-  font-weight: 300;
-  color: ${({ theme }) => theme.textColor};
-  svg {
-    width: 25px;
-    height: auto;
-    margin-right: 30px;
-  }
-`;
-
-const Refresh = styled.div`
-  position: absolute;
-  right: 15px;
-  bottom: 15px;
-  font-size: 12px;
-  display: inline-flex;
-  align-items: flex-end;
-  color: ${({ theme }) => theme.textColor};
-  svg {
-    margin-left: 10px;
-    width: 15px;
-    height: 15px;
-    cursor: pointer;
-    /* 使用 rotate 動畫效果 */
-    animation: rotate infinite 1.5s linear;
-    animation-duration: ${({ isLoading }) => (isLoading ? '1.5s' : '0s')};
-  }
-  @keyframes rotate {
-    from {
-      transform: rotate(360deg);
-    }
-    to {
-      transform: rotate(0deg);
-    }
-  }
-`;
 
 //api
 const AUTHORIZATION_KEY = 'CWB-F3A8D989-A19B-43F5-8FF5-09EC5C6B8FF7';
@@ -251,41 +155,14 @@ const App = () => {
     fetchData();
   }, [fetchData]);
   
-
-  //用解構賦值讓版面更乾淨
-  const {
-    observationTime,
-    locationName,
-    description,
-    windSpeed,
-    temperature,
-    rainPossibility,
-    comfortability,
-    station,
-    isLoading,
-    weatherCode,
-  } = weatherElement;
-
   return(
     <ThemeProvider theme={theme[currentTheme]}>
     <Container>
-      {console.log('render, isLoading: ', isLoading)}
-      <WeatherCard>
-        <Location>{locationName}</Location>
-        <Description>{description}{comfortability}</Description>
-        <CurrentWeather>
-          <Temperature>
-          {Math.round(temperature)} <Celsius>°C</Celsius>
-          </Temperature>
-          <WeatherIcon weatherCode={weatherCode} moment={moment} />
-        </CurrentWeather>
-        <AirFlow><AirFlowIcon /> {windSpeed} m/h </AirFlow>
-        <Rain><RainIcon /> {rainPossibility}% </Rain>
-        <Refresh onClick={fetchData} isLoading={isLoading}>
-          {station}站最後觀測時間：{new Intl.DateTimeFormat('zh-TW', {hour: 'numeric', minute: 'numeric',}).format(dayjs(observationTime))}
-          {isLoading ? <LoadingIcon /> : <RefreshIcon />}
-        </Refresh>
-      </WeatherCard>
+      <WeatherCard
+      weatherElement={weatherElement}
+      moment={moment}
+      fetchData={fetchData}
+      />
     </Container>
     </ThemeProvider>
   );
